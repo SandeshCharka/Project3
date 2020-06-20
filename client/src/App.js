9633f08e54d6ed0713fe631dfc5887491d902453
Ben Fung <benfung1996@gmail.com>
2020-06-20 16:05:27 -0700

    added login and register function in frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
+import Register from "./components/auth/Register";
+import Login from "./components/auth/Login";
+
 import Home from "./pages/Home";
 import NoMatch from "./pages/NoMatch";
 
@@ -12,6 +15,8 @@ class App extends Component {
     return (
       <Router>
         <Route exact path="/" component={Home} />
+        <Route exact path="/register" component={Register} />
+        <Route exact path="/login" component={Login} />
         <Route exact path="/noMatch" component={NoMatch} />
       </Router>
     );
