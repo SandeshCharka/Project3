6620ceccc2b88c8218f96bafddfeafbba52ddb66
Ben Fung <benfung1996@gmail.com>
2020-06-19 19:26:20 -0700

    completed backend login/register and validation function

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-    mongoURI: "mongodb+srv://benfung:Password1@userinfo-e92l1.mongodb.net/userinfo?retryWrites=true&w=majority" 
+    mongoURI: "mongodb+srv://benfung:Password1@userinfo-e92l1.mongodb.net/userinfo?retryWrites=true&w=majority",
+    secretOrKey: "secret"
   };
