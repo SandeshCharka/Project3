12114eea3e070094a5ccfa13dc0915708bedaa68
Ben Fung <benfung1996@gmail.com>
2020-06-20 21:52:33 -0700

    added redux

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./store";
 
 import Register from "./components/auth/Register";
 import Login from "./components/auth/Login";
@@ -13,12 +15,14 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <Router>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/register" component={Register} />
-        <Route exact path="/login" component={Login} />
-        <Route exact path="/noMatch" component={NoMatch} />
-      </Router>
+      <Provider store={store}>
+        <Router>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/register" component={Register} />
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/noMatch" component={NoMatch} />
+        </Router>
+      </Provider>
     );
   }
 }
