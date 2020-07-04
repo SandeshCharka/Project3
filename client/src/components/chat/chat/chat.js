1d33c87e38e1cc04c722866f038825158747601d
Ben Fung <benfung1996@gmail.com>
2020-07-04 14:58:19 -0700

    added users.js

--- client/src/components/chat/chat/chat.js
+++ client/src/components/chat/chat/chat.js
@@ -1,11 +1,36 @@
-import React, { Component } from 'react'
+import React, { useState, useEffect } from 'react';
+import queryString from "query-string";
+import io from "socket.io-client";
 
-export default class chat extends Component {
-    render() {
-        return (
-            <div>
-                Chat
-            </div>
-        )
-    }
+let socket;
+
+const Chat = ({ location }) => {
+    const [name, setName] = useState('');
+    const [room, setRoom] = useState('');
+    const EndPoint = "localhost:5000";
+
+    useEffect(() => {
+        const { name, room } = queryString.parse(location.search);
+
+        socket = io(EndPoint);
+
+        setName(name);
+        setRoom(room);
+
+        socket.emit("join", { name, room }, () => {
+        });
+
+        return () => {
+            socket.emit("disconnect");
+            socket.off();
+        }
+    }, [EndPoint, location.search]);
+
+    return (
+        <div>
+            Chat
+        </div>
+    )
 }
+
+export default Chat;
--- server.js
+++ server.js
@@ -12,7 +12,12 @@ const io = socketio(server);
 
 io.on("connection", (socket) => {
   console.log("we have a new connection!");
-  
+
+  socket.on("join", ({ name, room }, cb) => {
+    console.log(name, room);
+
+  })
+
   socket.on("disconnect", () => {
     console.log("User had left!");
   })
