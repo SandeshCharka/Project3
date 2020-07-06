3cbf8119bffcd7026208063554f43531c6a8160f
Ben Fung <benfung1996@gmail.com>
2020-07-05 18:57:12 -0700

    added messages display

--- client/src/components/chat/chat/chat.js
+++ client/src/components/chat/chat/chat.js
@@ -1,6 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import queryString from "query-string";
 import io from "socket.io-client";
+import InfoBar from "../InfoBar/InfoBar"
+import Input from "../Input/Input"
+import Messages from "../messages/messages";
+import "./chat.css"
 
 let socket;
 
@@ -46,10 +50,9 @@ const Chat = ({ location }) => {
     return (
         <div className="outerContainer">
             <div className="container">
-                <input value={message} 
-                onChange={(event) => setMessage(event.target.value)}
-                onKeyPress={event => event.key === "Enter" ? sendMessage(event) : null}
-                />
+                <InfoBar room={room}/>
+                <Messages messages={messages} name={name}/>
+                <Input message={message} setMessage={setMessage} sendMessage={sendMessage}/>
             </div>
         </div>
     )
